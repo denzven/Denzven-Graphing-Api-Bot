970c567e1063625557662bac879810e5adc3e800
Denzven <69366709+denzven@users.noreply.github.com>
2021-07-08 13:52:42 +0000

    Neatened code and added comments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Welcome! This is an Example Bot in Dicord.py,
 # that showcases the use of the DenzGraphingApi,
 # to form embeds and send basic info
-# you can invite the bot here: https://dsc.gg/denzgraphingapi-bot
+# you can invite the bot here: https://dsc.gg/denzven-graphing-api-bot
 # for doubts and queries Join the support/chill server: 
+# https://dsc.gg/chilly_place
 
 import discord
 from discord.ext import commands
@@ -10,9 +11,11 @@ import os
 import urllib.parse
 import aiohttp
 #from keep_alive import keep_alive
-from API_Formula_Examples import API_Formula_Examples
+#from API_Formula_Examples import API_Formula_Examples
 import datetime
 import Denzven_Graphing_Api_Wrapper as GraphingApi #pip install
+
+#################################################################################################################
 
 description = 'description'
 intents = discord.Intents.all()
@@ -25,21 +28,53 @@ bot = commands.Bot(
     strip_after_prefix=True,
     allowed_mentions=discord.AllowedMentions.none())
 
+ApiBaseUrl = "https://denzven.pythonanywhere.com"
+
+#################################################################################################################
+
 @bot.event
 async def on_connect():
 	print("the bot is ready")
 
 @bot.event
 async def on_ready():
-	print(f'We have logged in as {bot.user}')
+    global CommandNumber
+    CommandNumber = 0
+    print('\n\n\n')
+    print('+--------------------------------------------------+')
+    print('|                 Bot has Started                  |')
+    print('+--------------------------------------------------+')
+    print('| logged in as: {}   |'.format(bot.user))
+    print('+--------------------------------------------------+')
+    print('| No. of Servers: {}                                |'.format(len(bot.guilds)))
+    print('| Bot Prefix: ">"                                  |')
+    print('| Bot made by: @Denzven#2004                       |')
+    print('| Join my chill server: https://dsc.gg/chilly_place|')
+    print('+--------------------------------------------------+')
+    print('\n\n\n')
 
 @bot.event
 async def on_command(ctx):
-	server = ctx.guild.name
-	channel = ctx.channel
-	user = ctx.author
-	command = ctx.command
-	print(f'{server} > {channel} > {user} > {command}')
+    global CommandNumber
+    CommandNumber += 1
+    server = ctx.guild.name
+    channel = ctx.channel
+    user = ctx.author
+    command = ctx.command
+    print(f'\n\n\n')
+    print(f'+--------------------------------------------------+')
+    print(f'| {server} > {channel} > {user} > {command} ')
+    print(f'+--------------------------------------------------+')
+    print(f'| Bot-Usage: {bot.user}                     ')
+    print(f'+--------------------------------------------------+')
+    print(f'| Server: {server}                          ')
+    print(f'| Channel: {channel}                        ')
+    print(f'| User: {user}                              ')
+    print(f'| Command: {command}                        ') 
+    print(f'| Command content: {ctx.message.content}    ')
+    print(f'| Command Number: {CommandNumber}           ')
+    print(f'+--------------------------------------------------+')
+    print(f'\n\n\n')
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -62,122 +97,156 @@ async def on_command_error(ctx, error):
             return
         await ctx.reply(f"BotMissingPermissions **{' '.join(error.missing_perms[0].split('_')).title()}**")
 
-@bot.command()
-async def ping(ctx):
-	await ctx.send(f"ping ---> {round(bot.latency * 1000)} ms")
-
-@bot.command(
-    aliases = ['pyg']
-)
-async def pythonanywhere_graph(ctx, formula):
-    url = GraphingApi.py_anywhere_graph(formula)
-    await ctx.send(url)
-
-@bot.command(
-    aliases = ['hg']
-)
-async def heroku_graph(ctx, formula):
-    url = GraphingApi.heroku_graph(formula)
-    await ctx.send(url)
-
-@bot.command(
-    aliases = ['pyem']
-)
-async def pythonanywhere_graph_with_Embed(ctx, formula):
-    url = GraphingApi.py_anywhere_graph(formula)
-    embed = discord.Embed(title = f' the graphical representation of \n ```{formula} = 0```',color = discord.Color.green(),url = url)
-    embed.set_image(url = url)
-    embed.timestamp = datetime.datetime.utcnow()
-    embed.set_footer(text=f'rendered by {ctx.author.name}',icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=embed)
-
-@bot.command(
-    aliases = ['hgem']
-)
-async def heroku_graph_with_Embed(ctx, formula):
-    url = GraphingApi.heroku_graph(formula)
-    embed = discord.Embed(title = f' the graphical representation of \n ```{formula} = 0```',color = discord.Color.green(),url = url)
-    embed.set_image(url = url)
-    embed.timestamp = datetime.datetime.utcnow()
-    embed.set_footer(text=f'rendered by {ctx.author.name}',icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=embed)
+#################################################################################################################
 
 @bot.command(
     aliases = ['fgr']
 )
-async def async_file_graph(ctx,formula_input):
-    file = GraphingApi.async_file_graph(formula_input)
-    await ctx.send(file=discord.File(file))
-
-@bot.command(
-    aliases = ['pyb']
-)
-async def pythonanywhere_graph_beta(ctx, *, stuff):
-    python_anywhere_beta_BASEURL = "http://denzven.pythonanywhere.com/DenzGraphingApi/v1/flat_graph/test/plot"
-    #url = python_anywhere_beta_BASEURL + "http://denzven.pythonanywhere.com/DenzGraphingApi/v1/flat_graph/test/plot"
-    params = stuff.split(' ')
+async def Flat_graph(ctx, *, input_params):
+    ApiBaseUrl_FlatGraph = ApiBaseUrl + "/DenzGraphingApi/v1/flat_graph/test/plot"
+    params = input_params.split(' ')
     i = 0
     for e in params:
         if i == 0:
             e = urllib.parse.quote(e, safe='')
-            python_anywhere_beta_BASEURL = python_anywhere_beta_BASEURL + f"?formula={e}"
+            ReqUrl_Flat = ApiBaseUrl_FlatGraph + f"?formula={e}"
             i += 1
         if i != 0:
-            python_anywhere_beta_BASEURL = python_anywhere_beta_BASEURL + f"&{e}"
-    await ctx.send(python_anywhere_beta_BASEURL)
+            ReqUrl_Flat = ReqUrl_Flat + f"&{e}"
+    await ctx.reply(ReqUrl_Flat)
+
+############
 
 @bot.command(
-    aliases = ['pybem']
+    aliases = ['fgrem']
 )
-async def pythonanywhere_graph_beta_embed(ctx, *, stuff):
-    python_anywhere_beta_BASEURL = "http://denzven.pythonanywhere.com/DenzGraphingApi/v1/flat_graph/test/plot"
-    #url = python_anywhere_beta_BASEURL + "http://denzven.pythonanywhere.com/DenzGraphingApi/v1/flat_graph/test/plot"
-    params = stuff.split(' ')
+async def Flat_graph_embed(ctx, *, input_params):
+    ApiBaseUrl_FlatGraph = ApiBaseUrl + "/DenzGraphingApi/v1/flat_graph/test/plot"
+    params = input_params.split(' ')
     i = 0
     for e in params:
         if i == 0:
             e = urllib.parse.quote(e, safe='')
-            python_anywhere_beta_BASEURL = python_anywhere_beta_BASEURL + f"?formula={e}"
-        else:
-            python_anywhere_beta_BASEURL = python_anywhere_beta_BASEURL + f"&{e}"
-        i += 1
+            ReqUrl_Flat = ApiBaseUrl_FlatGraph + f"?formula={e}"
+            i += 1
+        if i != 0:
+            ReqUrl_Flat = ReqUrl_Flat + f"&{e}"
 
-    url = python_anywhere_beta_BASEURL
+    url = ReqUrl_Flat
     embed = discord.Embed(title = f'the graph',color = discord.Color.green(),url = url)
     embed.set_image(url = url)
     embed.timestamp = datetime.datetime.utcnow()
     embed.set_footer(text=f'rendered by {ctx.author.name}',icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=embed)
+    await ctx.reply(embed=embed)
+
+#################################################################################################################
+
+@bot.command(
+    aliases = ['pgr']
+)
+async def Polar_graph(ctx, *, input_params):
+    ApiBaseUrl_PolarGraph = ApiBaseUrl + "/DenzGraphingApi/v1/polar_graph/test/plot"
+    params = input_params.split(' ')
+    i = 0
+    for e in params:
+        if i == 0:
+            e = urllib.parse.quote(e, safe='')
+            ReqUrl_Polar = ApiBaseUrl_PolarGraph + f"?formula={e}"
+            i += 1
+        if i != 0:
+            ReqUrl_Polar = ReqUrl_Polar + f"&{e}"
+    await ctx.reply(ReqUrl_Polar)
+
+###############
+
+@bot.command(
+    aliases = ['pgrem']
+)
+async def Polar_graph_embed(ctx, *, input_params):
+    ApiBaseUrl_PolarGraph = ApiBaseUrl + "/DenzGraphingApi/v1/polar_graph/test/plot"
+    params = input_params.split(' ')
+    i = 0
+    for e in params:
+        if i == 0:
+            e = urllib.parse.quote(e, safe='')
+            ReqUrl_Polar = ApiBaseUrl_PolarGraph + f"?formula={e}"
+            i += 1
+        if i != 0:
+            ReqUrl_Polar = ReqUrl_Polar + f"&{e}"
+
+    url = ReqUrl_Polar
+    embed = discord.Embed(title = f'the graph',color = discord.Color.green(),url = url)
+    embed.set_image(url = url)
+    embed.timestamp = datetime.datetime.utcnow()
+    embed.set_footer(text=f'rendered by {ctx.author.name}',icon_url=ctx.author.avatar_url)
+    await ctx.reply(embed=embed)
+
+#################################################################################################################
+@bot.command(
+    aliases = ['3dgr']
+)
+async def threeD_graph(ctx, *, input_params):
+    ApiBaseUrl_3DGraph = ApiBaseUrl + "/DenzGraphingApi/v1/threeD_graph/test/plot"
+    params = input_params.split(' ')
+    i = 0
+    for e in params:
+        if i == 0:
+            e = urllib.parse.quote(e, safe='')
+            ReqUrl_3D = ApiBaseUrl_3DGraph + f"?formula={e}"
+            i += 1
+        if i != 0:
+            ReqUrl_3D = ReqUrl_3D + f"&{e}"
+    await ctx.reply(ReqUrl_3D)
+
+#############
+
+@bot.command(
+    aliases = ['3dgrem']
+)
+async def threeD_graph_embed(ctx, *, input_params):
+    ApiBaseUrl_3DGraph = ApiBaseUrl + "/DenzGraphingApi/v1/threeD_graph/test/plot"
+    params = input_params.split(' ')
+    i = 0
+    for e in params:
+        if i == 0:
+            e = urllib.parse.quote(e, safe='')
+            ReqUrl_3D = ApiBaseUrl_3DGraph + f"?formula={e}"
+            i += 1
+        if i != 0:
+            ReqUrl_3D = ReqUrl_3D + f"&{e}"
+
+    url = ReqUrl_3D
+    embed = discord.Embed(title = f'the graph',color = discord.Color.green(),url = url)
+    embed.set_image(url = url)
+    embed.timestamp = datetime.datetime.utcnow()
+    embed.set_footer(text=f'rendered by {ctx.author.name}',icon_url=ctx.author.avatar_url)
+    await ctx.reply(embed=embed)
+
+#################################################################################################################
 
 @bot.command()
-async def examples(ctx):
-    await ctx.send(API_Formula_Examples)
-
+async def ping(ctx):
+	await ctx.send(f"ping ---> {round(bot.latency * 1000)} ms")
 @bot.command()
 async def github(ctx):
-    await ctx.send("https://github.com/denzven/DenzGraphingApiWrapper_py")
-
+    await ctx.send("https://github.com/denzven/Denzven-Graphing-Api")
 @bot.command()
 async def docs(ctx):
-    await ctx.send("https://denzgraphingapiwrapper-py.readthedocs.io/en/latest/")
-
+    await ctx.send("https://denzven.pythonanywhere.com/docs")
 @bot.command()
 async def website(ctx):
     await ctx.send("https://denzven.pythonanywhere.com")
-
 @bot.command()
 async def pypi(ctx):
     await ctx.send("https://pypi.org/project/DenzGraphingApiWrapper-py/")
-
 @bot.command()
 async def test_pypi(ctx):
     await ctx.send("https://test.pypi.org/project/DenzGraphingApiWrapper-py-denzven/")
-
 @bot.command()
 async def code(ctx):
     await ctx.send(file = discord.File(r'main.py'), content = "Here is the Code of This Bot")
 
-#################################################
+#################################################################################################################
 
 #keep_alive()	
 bot.run(os.environ['bottoken'])
